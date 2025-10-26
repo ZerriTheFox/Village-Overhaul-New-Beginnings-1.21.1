bd67d5434e37e3948d353ce27b6406a4df343eb9
ZerriTheFox <manawind@gmail.com>
2025-10-26 12:45:33 -0500

    Initial

---
+++ src/main/java/net/zerrithefox/vonb/VillageOverhaulNewBeginnings.java
@@ -0,0 +1,16 @@
+package net.zerrithefox.vonb;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VillageOverhaulNewBeginnings implements ModInitializer {
+	public static final String MOD_ID = "village-overhaul-new-beginnings";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zerrithefox/vonb/VillageOverhaulNewBeginningsClient.java
@@ -0,0 +1,10 @@
+package net.zerrithefox.vonb;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillageOverhaulNewBeginningsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
